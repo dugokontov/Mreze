270e6eeb5c4401c35273cb7cec33442d282de5a5
Sergej Dugokontov <sergejspam@gmail.com>
2012-05-23 12:33:01 +0200

    Rails projekat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mreze::Application.config.secret_token = '30266b8dc68b06fafe41027775ad8bfe59668d5e6e3e0f93e19d3f7a4b3101dc2be709f366053b9f7262ad187e5de5a4a87a2b9867e811664a236facbe36acfa'
