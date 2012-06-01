322c3624f73846ead299e65605a376be1139327f
Sergej Dugokontov <sergejspam@gmail.com>
2012-06-01 14:01:56 +0200

    routing rules added in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,68 @@ c2 = s1.node.create({name: 'C2', node_type: Node::NODE_TYPES[0]})
 c2i1 = c2.interface.create({ip_address: '2002:B1A9:A883:2::1', protocol: Interface::PROTOCOLS[1], connected_to: r2i2})
 r2i2.connected_to = c2i1
 r2i2.save()
+
+RountingRule.delete_all
+RountingRule.create({
+  scenario_type: Scenario::SCENARIO_TYPES[0],
+  node_type: Node::NODE_TYPES[1],
+  protocol_in: Interface::PROTOCOLS[1],
+  protocol_out: Interface::PROTOCOLS[0],
+  script: %{function(message, node, interfaceInId) {
+    var getFromHex = function(hex) {
+        hex = ('0000' + hex).slice(-4);
+        return parseInt(hex.substring(0, 2), 16) + '.' + 
+            parseInt(hex.substring(2, 4), 16)
+    }
+    var dest_ip = "";
+    if (message.destination.substring(0,5) != '2002:') {
+        dest_ip = "192.88.99.1";
+    } else {
+        var splits = message.destination.split(':');
+        dest_ip = getFromHex(splits[1]) + '.' + getFromHex(splits[2])
+    }
+    var interfaceOut = node.interface
+        .findFirst(function() {return Number(this.id) !== Number(interfaceInId)});
+    var source_ip = interfaceOut.ip_address;
+    return new Message(source_ip, dest_ip, message);
+}}
+})
+
+RountingRule.create({
+  scenario_type: Scenario::SCENARIO_TYPES[0],
+  node_type: Node::NODE_TYPES[1],
+  protocol_in: Interface::PROTOCOLS[0],
+  protocol_out: Interface::PROTOCOLS[1],
+  script: %{function(message, node, interfaceInId) {
+    var interfaceIn = node.interface.findFirst(function() {
+        return Number(this.id) === Number(interfaceInId);
+    });
+    if (message.destination != interfaceIn.ip_address) {
+        throw node.name + " says: This message is for interface with IP address " + message.destination + ". Mine is: " + interfaceIn.ip_address;
+    }
+    if (!message.message) {
+        throw "Expecting package to contain IPv6 message. There is no IPv6 message.";
+    }
+    return message.message;
+}}
+})
+
+
+RountingRule.create({
+  scenario_type: Scenario::SCENARIO_TYPES[0],
+  node_type: Node::NODE_TYPES[2],
+  protocol_in: Interface::PROTOCOLS[0],
+  protocol_out: Interface::PROTOCOLS[1],
+  script: %{function(message, node, interfaceInId) {
+    var interfaceIn = node.interface.findFirst(function() {
+        return Number(this.id) === Number(interfaceInId);
+    });
+    if (message.destination != interfaceIn.ip_address && message.destination != '192.88.99.1') {
+        throw node.name + " says: This message is for interface with IP address " + message.destination + ". Mine is: " + interfaceIn.ip_address;
+    }
+    if (!message.message) {
+        throw "Expecting package to contain IPv6 message. There is no IPv6 message.";
+    }
+    return message.message;
+}}
+})
